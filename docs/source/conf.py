5fddc10d64a46336a9b6d167cadfdf000d94802b
Tom Yems <tyems.scouts@gmail.com>
2019-06-01 09:05:31 +0100

    Added more paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../..'))
 sys.path.insert(0, os.path.abspath('../../../..'))
 sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
