4da825f87c726fb0b1a7d00c39389d7a4fb03712
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2019-06-10 18:17:00 +0100

    clean add_ons_data_script

--- src/scripts/add_ons_data_script.py
+++ src/scripts/add_ons_data_script.py
@@ -9,11 +9,12 @@ This script has no command line options.
 from src.script_handler import ScriptHandler
 from src.scout_map import ScoutMap
 from src.ONS_data_May_18 import ONSDataMay18
+import json
 
 if __name__ == "__main__":
 
     script_handler = ScriptHandler(ons=False)
     with open("settings.json", "r") as read_file:
         settings = json.load(read_file)["settings"]
-    ons_data = ONSDataMay18(settings["ONS PD location""])
+    ons_data = ONSDataMay18(settings["ONS PD location"])
     script_handler.run(ScoutMap.create_ONS_lookup, [ons_data])
