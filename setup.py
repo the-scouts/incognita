a9c0c631874bde513e74d47b2a7ffe59511576bd
AA-Turner <9087854+AA-Turner@users.noreply.github.com>
2019-06-14 11:05:01 +0100

    Move to implicit namespace packages
    Remove __init__.py
    Switch find_packages to find_namespace_packages

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
-setup(name="geo_scout", packages=find_packages(), install_requires=['pandas', 'numpy', 'folium', 'branca'])
+setup(
+    name="geo_scout",
+    packages=find_namespace_packages(),
+    install_requires=['pandas', 'numpy', 'folium', 'branca', 'geopandas', 'shapely']
+)
