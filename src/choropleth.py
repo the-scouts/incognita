3cb6836be08420db2f4dd0ae5ff0290f3f2f40e8
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2019-06-26 19:46:34 +0100

    Improve & add documentation ChoroplethMapPlotter class

--- src/choropleth.py
+++ src/choropleth.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import folium
-# from folium.plugins import MarkerCluster
+from folium.plugins import MarkerCluster
 import geopandas
 import webbrowser
 import os
@@ -10,52 +10,59 @@ import src.log_util as log_util
 # It is used by folium to plot the data.
 WGS_84 = '4326'
 
-# ------------------------------------------------------------------------------
-# Class ChoroplethMapPlotter
-#
-# This class enables easy plotting of maps with a shape file. It makes the
-# following assumptions:
-#
-# The inputted csv_file must have the first column 'Geo_code' and the second
-# column 'Score'. The 'Geo_code' column must have values which match with the
-# 'code' property of the GeoJSON (which has been converted from the shape file)
-#
-# ------------------------------------------------------------------------------
-
 
 class ChoroplethMapPlotter:
-    def __init__(self, shape_files, data_info, out_file):
+    """This class enables easy plotting of maps with a shape file.
+
+    :param dict shape_files_dict: dictionary of properties about the needed shapefiles
+    :param data_info: dictionary of information about ???
+    :param str out_file: path to save the map to
+
+    :var dictionary self.map_data: contains shapefile paths, and labels for region codes and names
+    :var str self.CODE_COL: holds the name of the region class, e.g. oslaua, pcon
+    :var self.SCORE_COL: holds the column name of the choropleth dimension
+    :var self.score_col_label: tooltip label for the self.SCORE_COL value
+    :var self.map: holds the folium map object
+    """
+
+    def __init__(self, shape_files_dict, data_info, out_file):
         # Facilitates logging
         self.logger = log_util.create_logger(__name__,)
 
         self.out_file = out_file + ".html"
-        self.code_name = shape_files["key"]
+        self.code_name = shape_files_dict["key"]
 
         self.map_data = data_info['data']
         self.CODE_COL = data_info['code_col']
         self.SCORE_COL = data_info['score_col']
-        self.display_score_col = data_info['display_score_col']
+        self.score_col_label = data_info["score_col_label"]
 
         # Create folium map
-        self.map = folium.Map(location=[53.5,-1.49],zoom_start=6)
+        self.map = folium.Map(location=[53.5, -1.49], zoom_start=6)
 
         # self.marker_cluster = MarkerCluster(name='Sections').add_to(self.map)
-        self.shape_file_paths = shape_files["shapefiles"]
+        self.shape_file_paths = shape_files_dict["shapefiles"]
         self.filter_shape_file(self.shape_file_paths)
 
         self.geo_data = None
 
     def filter_shape_file(self, shape_file_paths):
-        # Load ShapeFiles
-        # Convert data from National Grid easting/northing to WGS84 co-ords
-        # output data to geoJSON
+        """Loads, filters and converts shapefiles for later use
+
+        Loads shapefiles from paths into GeoPandas dataframe
+        Filters out unneeded shapes within all shapes loaded
+        Converts from British National Grid to WGS84, as Leaflet doesn't understand BNG
+
+        :param list shape_file_paths: list of paths to ESRI shapefile files with region information
+        :return: None
+        """
 
         # Read a shape file
         data_frames = []
         for shape_file_path in shape_file_paths:
             data_frames.append(geopandas.GeoDataFrame.from_file(shape_file_path))
 
-        all_shapes = pd.concat(data_frames,sort=False)
+        all_shapes = pd.concat(data_frames, sort=False)
 
         original_number_of_shapes = len(all_shapes.index)
         self.logger.info(f"Filtering {original_number_of_shapes} shapes by {self.code_name} being in the {self.CODE_COL} of the map_data")
@@ -70,18 +77,43 @@ class ChoroplethMapPlotter:
         # self.logger.debug(f"geo_data\n{self.geo_data}")
 
     def plot(self, name, show, boundary_name, colormap):
+        """Adds features from self.geo_data to map
+
+        :param str name: the name of the Layer, as it will appear in the layer controls
+        :param bool show: whether to show the layer by default
+        :param str boundary_name: column heading for human-readable region name
+        :param colormap: branca colour map object
+        :return: None
+        """
         # self.logger.debug(f"Merging geo_json on {self.code_name} with {self.CODE_COL} from boundary report")
         merged_data = self.geo_data.merge(self.map_data, left_on=self.code_name, right_on=self.CODE_COL)
         # self.logger.debug(f"Merged_data\n{merged_data}")
 
-        folium.GeoJson(merged_data.to_json(),
-                       name=name,
-                       style_function=lambda x: {'fillColor':self.my_colormap(x['properties'], colormap), 'color':'black','fillOpacity':0.4, 'weight':0.2},
-                       tooltip=folium.GeoJsonTooltip(fields=[boundary_name ,self.SCORE_COL], aliases=['Name', self.display_score_col], localize=True),
-                       show=show).add_to(self.map)
+        folium.GeoJson(
+            data=merged_data.to_json(),
+            name=name,
+            style_function=lambda x: {
+               'fillColor': self.my_colormap(x['properties'], colormap),
+               'color': 'black',
+               'fillOpacity': 0.4,
+               'weight': 0.2
+            },
+            tooltip=folium.GeoJsonTooltip(
+               fields=[boundary_name, self.SCORE_COL],
+               aliases=['Name', self.score_col_label],
+               localize=True
+            ),
+            show=show
+        ).add_to(self.map)
         colormap.add_to(self.map)
 
     def my_colormap(self, properties, colormap):
+        """Returns colour from colour map function and value
+
+        :param properties: dictionary of properties
+        :param colormap: a Branca Colormap object to calculate the region's colour
+        :return str: hexadecimal colour value "#RRGGBB"
+        """
         area_score = properties[self.SCORE_COL]
         if area_score is None:
             return '#cccccc'
@@ -91,6 +123,14 @@ class ChoroplethMapPlotter:
             return colormap(area_score)
 
     def add_marker(self, lat, long, popup, color):
+        """Adds a leaflet marker to the map using given values
+
+        :param float lat: latitude of the marker
+        :param float long: longitude of the marker
+        :param folium.Popup popup: popup text for the marker
+        :param string color: colour for the marker
+        :return: None
+        """
         folium.Marker(
             location=[lat, long],
             popup=popup,
@@ -99,9 +139,11 @@ class ChoroplethMapPlotter:
         # ).add_to(self.marker_cluster)
 
     def save(self):
+        """Saves the folium map to a HTML file """
         # Add layer control to map
         folium.LayerControl(collapsed=False).add_to(self.map)
         self.map.save(self.out_file)
 
     def show(self):
+        """Show the file at self.out_file in the default browser. """
         webbrowser.open("file://" + os.path.realpath(self.out_file))
