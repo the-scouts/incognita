9d8c63ad3a9eba256c381d9b7d99a7d1ac96ee7e
Tom Yems <tyems.scouts@gmail.com>
2019-07-01 21:40:04 +0100

    Enables custom layer creation on map

--- src/map_plotter.py
+++ src/map_plotter.py
@@ -42,15 +42,19 @@ class ChoroplethMapPlotter:
         # Create folium map
         self.map = folium.Map(location=[53.5, -1.49], zoom_start=6)
 
-        if sections_clustered:
-            self.section_layer = MarkerCluster(name='Sections').add_to(self.map)
-        else:
-            self.section_layer = FeatureGroup(name='Sections').add_to(self.map)
+        self.layers = {}
+        self.add_layer('Sections', sections_clustered)
 
         self.shape_file_paths = shape_files_dict["shapefiles"]
         self.geo_data = None
 
         self.filter_shape_file(self.shape_file_paths)
+
+    def add_layer(self, name, markers_clustered):
+        if markers_clustered:
+            self.layers[name] = MarkerCluster(name=name).add_to(self.map)
+        else:
+            self.layers[name] = FeatureGroup(name=name).add_to(self.map)
 
     def filter_shape_file(self, shape_file_paths):
         """Loads, filters and converts shapefiles for later use
