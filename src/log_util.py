6f7ca099e523eb699620964a257cecab3d47edb3
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2019-07-23 22:01:59 +0100

    Remove unused logging methods

--- src/log_util.py
+++ src/log_util.py
@@ -46,32 +46,3 @@ class LogUtil:
     def finished_message(self, action, name=None, start_time=None):
         ending = f" in {name}." if name else "."
         self.logger.info(f"{action} finished. {self.duration(start_time):.2f} seconds elapsed{ending}")
-
-
-def create_logger(name, file_path=None):
-    """Creates and returns a logger with preset options
-
-    :param str name: name to give the logger
-    :param file_path: file path to output debug log
-    :return: logger object
-    """
-    # set up a log to file
-    if file_path:
-        logging.basicConfig(filename=file_path, level=logging.DEBUG, filemode="w")
-
-    # set up a log to the console
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    console.setFormatter(logging.Formatter(fmt="%(name)s - %(levelname)s - %(message)s"))
-
-    # creates the main logger
-    logger = logging.getLogger(name)
-    # add the handler to the root logger
-    logger.addHandler(console)
-
-    return logger
-
-
-def duration(start_time):
-    """Returns elapsed time since given start time"""
-    return time.time() - start_time
