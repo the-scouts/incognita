f317a8e7b09c24b5c8ec58e90c86d28baace1d3d
Tom Yems <tyems.scouts@gmail.com>
2019-07-23 20:26:48 +0100

    Added comments to District boundaries and tidied redundant code

--- src/scout_map.py
+++ src/scout_map.py
@@ -1298,41 +1298,63 @@ class ScoutMap:
         return output_pd
 
     def create_district_boundaries(self):
+        """
+        Creates a GeoJSON file for the District Boundaries of the Scout Census.
+
+        Aims to create a circular boundary around every section of maximal size
+        that doesn't overlap or leave gaps between Districts.
+        """
+
         if not self.has_ons_data():
             raise Exception("Must have ons data added before creating district boundaries")
+
+        # Find all the District IDs and names
         districts = self.census_data.data[[CensusData.column_labels['id']["DISTRICT"], CensusData.column_labels['name']["DISTRICT"]]].drop_duplicates()
 
+        # Finds all the records with valid postcodes in the Scout Census
         valid_locations = self.census_data.data.loc[self.census_data.data[CensusData.column_labels['VALID_POSTCODE']] == 1]
+
+        # Creates a new dataframe with a subset of columns resulting in
+        # each location being a distinct row
         all_locations = pd.DataFrame(columns=["D_ID", "D_name", "lat", "long"])
         all_locations[["D_name", "Object_ID"]] = valid_locations[["D_name", "Object_ID"]]
         all_locations[["D_ID", "lat", "long"]] = valid_locations[["D_ID", "lat", "long"]].apply(pd.to_numeric, errors='coerce')
         all_locations.drop_duplicates(subset=["lat", "long"], inplace=True)
+
+        # Uses the lat and long co-ords from above to create a GeoDataFrame
         all_points = gpd.GeoDataFrame(all_locations, geometry=gpd.points_from_xy(all_locations.long, all_locations.lat))
         all_points.crs = {'init': 'epsg:4326'}
+
+        # Converts the co-ordinate reference system into OS36 which uses
+        # (x-y) coordinates in metres, rather than (long, lat) coordinates.
         all_points = all_points.to_crs({'init': 'epsg:27700'})
         all_points.reset_index(inplace=True)
 
         self.logger.info(f"Found {len(all_points.index)} different Section points")
 
-        #row = all_points.iloc[0]
-        #test_point = self.nearest_other_points(row, all_points.loc[all_points["D_ID"] != row["D_ID"]])
-        #self.logger.debug(f"Found nearest points as\n{test_point}")
+        # Calculates all the other points within twice the distance of the
+        # closest point from a neighbouring district
         all_points["nearest_points"] = all_points.apply(lambda row: self.nearest_other_points(row, all_points.loc[all_points["D_ID"] != row["D_ID"]]), axis=1)
         all_points["buffer_distance"] = 0
+
+        # Initial calcuation of the buffer distances
         self.logger.info("Calculating buffer distances of " + str(all_points["buffer_distance"].value_counts().iloc[0]) + " points")
-        all_points["buffer_distance"] = all_points.apply(lambda row: self.second_buffer_distance(row, all_points), axis=1)
+        all_points["buffer_distance"] = all_points.apply(lambda row: self.buffer_distance(row, all_points), axis=1)
         self.logger.info("On first pass " + str(all_points["buffer_distance"].value_counts().iloc[0]) + " missing buffer distance")
 
         old_number = all_points["buffer_distance"].value_counts().iloc[0]
         new_number = 0
+        # The algorithm is iterative, so stop when no more points have their
+        # buffers identified
         while new_number < old_number:
             old_number = all_points["buffer_distance"].value_counts().iloc[0]
-            all_points["buffer_distance"] = all_points.apply(lambda row: self.second_buffer_distance(row, all_points), axis=1)
+            all_points["buffer_distance"] = all_points.apply(lambda row: self.buffer_distance(row, all_points), axis=1)
             new_number = all_points["buffer_distance"].value_counts().iloc[0]
             self.logger.info(f"On next pass {new_number} missing buffer distance")
             _ = all_points.loc[all_points["buffer_distance"] == 0]
             self.logger.debug(f"The following points do not have buffer distances defined:\n{_}")
 
+        # Create the GeoDataFrame that will form the GeoJSON
         output_columns = ["id", "name"]
         output_gpd = gpd.GeoDataFrame(columns=output_columns)
         district_nu = 0
@@ -1346,36 +1368,31 @@ class ScoutMap:
                 self.logger.info(f"{district_nu}/{len(districts)} calculating boundary of {district['D_name']}")
 
                 district_points = all_points.loc[all_points["D_ID"] == district["D_ID"]]
+
+                # For each of the points in the district, produces a polygon to
+                # represent the buffered point from the buffer distances
+                # calculated above
                 buffered_points = district_points.apply(lambda row: row["geometry"].buffer(row["buffer_distance"]), axis=1)
+
+                # Unifies the polygons created from each point in the District
+                # into one polygon for the District.
                 district_polygon = shapely.ops.unary_union(buffered_points)
 
                 data_df = gpd.GeoDataFrame(data, columns=output_columns, geometry=[district_polygon])
                 output_gpd = gpd.GeoDataFrame(pd.concat([output_gpd, data_df], axis=0, sort=False))
 
         output_gpd.crs = {'init': 'epsg:27700'}
+
+        # Convert co-ordinates back to WGS84, which uses latitude and longditude
         output_gpd = output_gpd.to_crs({'init': 'epsg:4326'})
         output_gpd.reset_index(drop=True, inplace=True)
+
         self.logger.debug(f"output gpd\n{output_gpd}")
         output_gpd[["id"]] = output_gpd[["id"]].apply(pd.to_numeric, errors='coerce')
         self.logger.debug(f"output gpd\n{output_gpd}")
         output_gpd.to_file("districts_buffered.geojson", driver='GeoJSON')
 
-    @staticmethod
-    def simple_buffer_distance(point_details, all_points):
-        nearest_point = point_details["nearest_points"][0]["Point"]
-        nearest_point_details = all_points.loc[all_points["geometry"] == nearest_point]
-
-        if not nearest_point_details.empty:
-            if point_details["geometry"] == nearest_point_details["nearest_points"].iloc[0][0]["Point"]:
-                distance = point_details["nearest_points"][0]["Distance"] / 2
-            else:
-                distance = 0
-        else:
-            distance = 0
-
-        return distance
-
-    def second_buffer_distance(self, point_details, all_points):
+    def buffer_distance(self, point_details, all_points):
         obj_id = point_details["Object_ID"]
         self.logger.debug(f"Finding buffer distance of {point_details.index} with object ID {obj_id}")
         distance = 0
@@ -1421,7 +1438,7 @@ class ScoutMap:
             if not valid:
                 distance = 0
         else:
-            distance = point_details["buffer_distance"]
+            distance = 0
         return distance
 
     @staticmethod
