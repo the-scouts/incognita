64a2ab7f41d5dd6cecee8f708c8ef937949a241f
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2020-03-31 19:09:57 +0100

    Clean ONS PD reduce file

--- src/scripts/setup_reduce_onspd.py
+++ src/scripts/setup_reduce_onspd.py
@@ -1,8 +1,10 @@
 from data.ons_pd_may_19 import ONSPostcodeDirectoryMay19
 
-print("starting")
-ons_pd = ONSPostcodeDirectoryMay19("../../data/ONSPD_MAY_2019_UK/Data/ONSPD_MAY_2019_UK.csv", load_data=True)
-a = ons_pd.data
-b = a.reset_index(drop=True)
-c = b[['oscty', 'oslaua', 'osward', 'ctry', 'rgn', 'pcon', 'lsoa11', 'msoa11', 'imd']].drop_duplicates()
-c.to_csv("ONSPD_reduced_no_postcodes_or_co_ordinates.csv", index=False, encoding='utf-8-sig')
+if __name__ == "__main__":
+    print("Starting")
+    ons_pd = ONSPostcodeDirectoryMay19("../../data/ONSPD_MAY_2019_UK/Data/ONSPD_MAY_2019_UK.csv", load_data=True)
+    print("Loaded data")
+    data = ons_pd.data.reset_index(drop=True)
+    reduced_data = data[['oscty', 'oslaua', 'osward', 'ctry', 'rgn', 'pcon', 'lsoa11', 'msoa11', 'imd']].drop_duplicates()
+    print("Saving data")
+    reduced_data.to_csv("ONSPD_reduced_no_postcodes_or_co_ordinates.csv", index=False, encoding='utf-8-sig')
