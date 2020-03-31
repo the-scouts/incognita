05acebae2329bbcdc8a8e2e31392eec00c9c456d
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2020-04-01 00:55:20 +0100

    Preparing for Travis CI

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="geo_mapping",
+    url="https://github.com/the-scouts/geo_mapping",
     packages=find_namespace_packages(),
-    install_requires=['pandas', 'numpy', 'folium', 'branca', 'geopandas', 'shapely', ]
+    install_requires=['pandas', 'numpy', 'folium', 'branca', 'geopandas', 'shapely', 'dash'],
+    extras_require={
+        'dev': ['pytest', 'pytest-cov'],
+    }
 )
