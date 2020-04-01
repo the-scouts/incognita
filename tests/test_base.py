08f5defb1c560d0fa83d25a70149222b2b9111d5
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2020-04-01 19:50:59 +0100

    Use pytest fixtures

--- tests/test_base.py
+++ tests/test_base.py
@@ -28,6 +28,20 @@ class ExampleClassSettings(Base):
         super().__init__(settings=True)
 
 
+@pytest.fixture
+def ec_logger():
+    """Returns an ExampleClassLogger instance"""
+    def _instantiator(path=True):
+        return ExampleClassLogger(path)
+    return _instantiator
+
+
+@pytest.fixture
+def ec_settings():
+    """Returns an ExampleClassSettings instance"""
+    return ExampleClassSettings()
+
+
 def test_time_function_wraps_function():
     assert time_function(example_function)(2, 2) == example_function(2, 2)
 
@@ -38,6 +52,17 @@ def test_time_function_raises_exception_on_non_method_arguments():
         time_function('not a function or method')
 
 
+def test_base_open_settings(ec_settings):
+    assert isinstance(ec_settings.settings, dict)
+
+
+def test_base_settings_are_accurate(ec_settings):
+    with open(SCRIPTS_ROOT.joinpath("settings.json"), "r") as read_file:
+        settings = json.load(read_file)["settings"]
+
+    assert ec_settings.settings == settings
+
+
 def test_time_function_no_logger_entity():
     try:
         time_function(example_function)(2, 2)
@@ -45,41 +70,28 @@ def test_time_function_no_logger_entity():
         pytest.fail(f"Unexpected AttributeError in base.test_function")
 
 
-def test_time_function_logger_output(caplog):
+def test_time_function_logger_output(caplog, ec_logger):
     caplog.set_level(logging.INFO)
-    ExampleClassLogger().example_function(2, 2)
+    ec_logger().example_function(2, 2)
 
     assert 'Calling function example_function' in caplog.text
     assert 'example_function took 0.0' in caplog.text
 
 
-def test_base_open_settings():
-    assert isinstance(ExampleClassSettings().settings, dict)
+def test_base_logger_creation(ec_logger):
+    assert isinstance(ec_logger().logger, logging.Logger)
 
 
-def test_base_settings_are_accurate():
-    with open(SCRIPTS_ROOT.joinpath("settings.json"), "r") as read_file:
-        settings = json.load(read_file)["settings"]
-
-    assert ExampleClassSettings().settings == settings
-
-
-def test_base_logger_creation():
-    ec = ExampleClassLogger()
-    assert isinstance(ec.logger, logging.Logger)
-
-
-def test_base_logger_retrieval():
-    ec0 = ExampleClassLogger(path=True)
-    ec1 = ExampleClassLogger(path=False)
+def test_base_logger_retrieval(ec_logger):
+    ec0 = ec_logger(path=True)
+    ec1 = ec_logger(path=False)
 
     assert ec1.logger is not None
     assert ec0.logger == ec1.logger
 
 
-def test_base_close_script(caplog):
+def test_base_close_script(caplog, ec_logger):
     caplog.set_level(logging.INFO)
-    ec = ExampleClassLogger()
-    ec.close()
+    ec_logger().close()
 
     assert 'Script finished, 0.00 seconds elapsed.' in caplog.text
