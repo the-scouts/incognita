4ab610ae3212416d938b364955a0f5972ea9d0b4
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2020-04-04 21:38:59 +0100

    Setup pre-commit

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     version="2.0.0",
     packages=find_namespace_packages(),
     install_requires=["pandas", "numpy", "folium", "branca", "geopandas", "shapely", "dash"],
-    extras_require={"dev": ["pytest", "pytest-cov"],},
+    extras_require={"dev": ["pytest", "pytest-cov", "pre-commit"]},
     python_requires=">=3.7",
 )
