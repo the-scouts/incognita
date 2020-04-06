bf3cec6205c12cca21fdc27428234c3c9b15de44
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2020-04-06 16:17:34 +0100

    Fixes to full/reduced paths

--- scripts/setup_data_file.py
+++ scripts/setup_data_file.py
@@ -11,6 +11,6 @@ from src.data.ons_pd_may_19 import ONSPostcodeDirectoryMay19
 
 if __name__ == "__main__":
     scout_data = ScoutData(csv_has_ons_pd_data=False)
-    ons_pd = ONSPostcodeDirectoryMay19(scout_data.settings["ONS PD location"])
+    ons_pd = ONSPostcodeDirectoryMay19(scout_data.settings["Full ONS PD location"])
     scout_data.merge_ons_postcode_directory(ons_pd)
     scout_data.close()
