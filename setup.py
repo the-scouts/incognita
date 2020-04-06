ae6d26e1fb701e2db0499732e61a8227839ee9c4
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2020-04-06 04:25:41 +0100

    Correct version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="geo_mapping",
     url="https://github.com/the-scouts/geo_mapping",
-    version="2.0.0",
+    version="0.2.0",
     packages=find_namespace_packages(),
     install_requires=["pandas", "numpy", "folium", "branca", "geopandas", "shapely", "dash"],
     extras_require={"dev": ["pytest", "pytest-cov", "pre-commit"]},
