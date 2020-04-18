68a4440f46b2ed9d343fdd3f8ce30df1b354590d
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2020-04-18 19:41:42 +0100

    Tidy map_county_imd_map.py

--- scripts/map_county_imd_map.py
+++ scripts/map_county_imd_map.py
@@ -1,24 +1,30 @@
 from src.data.scout_data import ScoutData
-from src.maps.map import Map
 from src.reports.reports import Reports
+from src.maps.map import Map
 
 if __name__ == "__main__":
+    county_name = "Shropshire"
+    county_name2 = "West Mercia"
+
     scout_data = ScoutData(load_ons_pd_data=True)
-    scout_data.filter_records("C_ID", [10000111, 10000119])
-    scout_data.filter_records("Year", [2015, 2016, 2017, 2018, 2019])
+    scout_data.filter_records("C_Name", [county_name, county_name2])
+    scout_data.filter_records("Year", [2016, 2017, 2018, 2019, 2020])
     scout_data.filter_records("postcode_is_valid", [1])
 
-    map = Map(scout_data, map_name="shropshire")
-
-    dimension = {"column": "imd_decile", "tooltip": "IMD", "legend": "IMD Decile"}
     reports = Reports("lsoa", scout_data)
-    reports.filter_boundaries("C_ID", [10000111, 10000119], "oslaua")
-    reports.create_boundary_report(["Section numbers"], historical=True, report_name="shropshire_by_lsoa")  # TODO: before postcode filtering
-    map.add_areas(dimension, reports, show=True)
+    reports.filter_boundaries("C_Name", [county_name, county_name2], "oslaua")
+    reports.create_boundary_report(["Section numbers"], historical=True, report_name=f"{county_name}_by_lsoa")  # TODO: before postcode filtering
 
-    map.set_region_of_colour("C_ID", [10000111])
-    map.add_sections_to_map(scout_data, map.district_colour_mapping(), ["youth membership"])
+    # Create map object
+    mapper = Map(scout_data, map_name=f"{county_name}")
 
-    map.save_map()
-    map.show_map()
+    # Plot
+    dimension = {"column": "imd_decile", "tooltip": "IMD", "legend": "IMD Decile"}
+    mapper.add_areas(dimension, reports, show=True)
+    mapper.set_region_of_colour("C_Name", [county_name])
+    mapper.add_sections_to_map(scout_data, mapper.district_colour_mapping(), ["youth membership"])
+
+    # Save the map and display
+    mapper.save_map()
+    mapper.show_map()
     scout_data.close()
