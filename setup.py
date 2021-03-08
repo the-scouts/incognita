ab7daf06a0d14840c72e2a2678c1bf8a5d8b7ec6
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2021-03-08 21:31:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url="https://github.com/the-scouts/geo_mapping",
     version="0.2.0",
     packages=find_namespace_packages(),
-    install_requires=["pandas", "numpy", "folium", "branca", "geopandas", "shapely", "dash", "pyarrow"],
+    install_requires=["pandas", "numpy", "folium", "branca", "geopandas", "shapely", "dash", "pyarrow", "pygeos"],
     extras_require={"dev": ["pytest", "hypothesis", "pytest-cov", "pre-commit", "black"]},
     python_requires=">=3.8",
 )
