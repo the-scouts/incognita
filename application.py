a51d95942f2f01ef2f7de50a52fc75eef70a67e3
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2021-03-11 01:13:51 +0000

    Move dashboard code to application.py

--- application.py
+++ application.py
@@ -1,8 +1,26 @@
-from incognita.maps.dashboard import Dashboard
+from pathlib import Path
 
-dashboard = Dashboard()
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+dashboard = dash.Dash(__name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
+
+dashboard.layout = html.Div(
+    [
+        html.H1("Central Yorkshire Scouts Uptake and IMD Decile Map"),
+        html.Iframe(
+            id="map",
+            width="100%",
+            height="600",
+            srcDoc=Path("Example Output/central_yorkshire_uptake_imd_map.html").read_text(),
+        ),
+    ]
+)
+
+dashboard.input_layout = html.Div([dcc.Upload(id="upload-scout-data", children=html.Div(["Add Census with ONS Postcode Directory", html.A("Select Files")]))])
 
 if __name__ == "__main__":
-    dashboard.run()
+    dashboard.run_server(debug=False)
 else:
-    app = dashboard.app.server
+    app = dashboard.server
