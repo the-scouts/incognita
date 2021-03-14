5f8a4a2e54d2b9415f2d212dd3d6d6ff884ffe46
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2021-03-14 02:37:13 +0000

    Move ScoutData merge functions to setup_data_file.py

--- src/incognita/data/scout_data.py
+++ src/incognita/data/scout_data.py
@@ -8,14 +8,12 @@ from typing import TYPE_CHECKING
 import geopandas as gpd
 import pandas as pd
 
-from incognita.preprocessing.census_merge_data import CensusMergeData
 from incognita.data.ons_pd_may_19 import ONSPostcodeDirectoryMay19
 from incognita.data.scout_census import ScoutCensus
 from incognita.logger import logger
 from incognita.utility import config
 from incognita.utility import utility
 
-# type hints
 if TYPE_CHECKING:
     from incognita.data.ons_pd import ONSPostcodeDirectory
 
@@ -61,83 +59,6 @@ class ScoutData:
                 raise Exception(f"The ScoutCensus file has no ONS data, because it doesn't have a {ScoutCensus.column_labels['VALID_POSTCODE']} column")
 
             logger.info(f"Loading {self.ons_pd.PUBLICATION_DATE} ONS Postcode data finished, {time.time() - start_time:.2f} seconds elapsed.")
-
-    def merge_ons_postcode_directory(self, ons_pd: ONSPostcodeDirectory) -> None:
-        """Merges census extract data with ONS data
-
-        Args:
-            ons_pd: Refers to the ONS Postcode Director
-
-        """
-        ons_fields_data_types = {
-            "categorical": ["lsoa11", "msoa11", "oslaua", "osward", "pcon", "oscty", "ctry", "rgn"],
-            "int": ["oseast1m", "osnrth1m", "lat", "long", "imd"],
-        }
-
-        logger.debug("Initialising merge object")
-        merge = CensusMergeData()
-
-        logger.info("Cleaning the postcodes")
-        merge.clean_and_verify_postcode(self.data, ScoutCensus.column_labels["POSTCODE"])
-
-        logger.info("Adding ONS postcode directory data to Census and outputting")
-
-        # initially merge just Country column to test what postcodes can match
-        self.data = merge.merge_data(self.data, ons_pd.data["ctry"], "clean_postcode")
-
-        # attempt to fix invalid postcodes
-        self.data = merge.try_fix_invalid_postcodes(self.data, ons_pd.data["ctry"])
-
-        # fully merge the data
-        self.data = merge.merge_data(self.data, ons_pd.data, "clean_postcode")
-
-        # fill unmerged rows with default values
-        logger.info("filling unmerged rows")
-        self.data = merge.fill_unmerged_rows(self.data, ScoutCensus.column_labels["VALID_POSTCODE"], ons_fields_data_types)
-
-        # Filter to useful columns
-        # fmt: off
-        self.data = self.data[[
-            "Object_ID", "compass", "type", "name", "G_ID", "G_name", "D_ID", "D_name", "C_ID", "C_name", "R_ID", "R_name", "X_ID", "X_name",
-            "postcode", "clean_postcode", "postcode_is_valid", "Year", "Beavers_Units", "Cubs_Units", "Scouts_Units", "Explorers_Units", "Network_Units", "Young_Leader_Unit",
-            "Beavers_f", "Beavers_m", "Beavers_total", "Cubs_f", "Cubs_m", "Cubs_total", "Scouts_f", "Scouts_m", "Scouts_total", "Explorers_f", "Explorers_m", "Explorers_total",
-            "Network_f", "Network_m", "Network_total", "Yls", "WaitList_b", "WaitList_c", "WaitList_s", "WaitList_e", "Leaders", "AssistantLeaders", "SectAssistants", "OtherAdults",
-            "Chief_Scout_Bronze_Awards", "Chief_Scout_Silver_Awards", "Chief_Scout_Gold_Awards", "Chief_Scout_Platinum_Awards", "Chief_Scout_Diamond_Awards",
-            "Duke_Of_Edinburghs_Bronze", "Duke_Of_Edinburghs_Silver", "Duke_Of_Edinburghs_Gold", "Young_Leader_Belts", "Explorer_Belts", "ScoutsOfTheWorldAward", "Queens_Scout_Awards",
-            "Eligible4Bronze", "Eligible4Silver", "Eligible4Gold", "Eligible4Diamond", "Eligible4QSA", "Eligible4SOWA",
-            "oscty", "oslaua", "osward", "ctry", "rgn", "pcon", "lsoa11", "msoa11", "lat", "long", "imd"
-        ]]
-        # fmt: on
-
-        # Add IMD decile column
-        self.data["imd_decile"] = utility.calc_imd_decile(self.data["imd"], self.data["ctry"], ons_pd).astype("UInt8")
-
-    def save_merged_data(self, ons_pd_publication_date: str) -> None:
-        """Save passed dataframe to csv file.
-
-        Also output list of errors in the merge process to a text file
-
-        Args:
-            ons_pd_publication_date: Refers to the ONS Postcode Directory's publication date
-
-        """
-        raw_extract_path = config.SETTINGS.census_extract.original
-        output_path = raw_extract_path.parent / f"{raw_extract_path.stem} with {ons_pd_publication_date} fields"
-        error_output_path = config.SETTINGS.folders.output / "error_file.csv"
-
-        valid_postcode_label = ScoutCensus.column_labels["VALID_POSTCODE"]
-        postcode_merge_column = "clean_postcode"
-        original_postcode_label = ScoutCensus.column_labels["POSTCODE"]
-        compass_id_label = ScoutCensus.column_labels["id"]["COMPASS"]
-
-        # The errors file contains all the postcodes that failed to be looked up in the ONS Postcode Directory
-        error_output_fields = [postcode_merge_column, original_postcode_label, compass_id_label, "type", "name", "G_name", "D_name", "C_name", "R_name", "X_name", "Year"]
-        self.data.loc[self.data[valid_postcode_label] == 0, error_output_fields].to_csv(error_output_path, index=False, encoding="utf-8-sig")
-
-        # Write the new data to a csv file (utf-8-sig only to force excel to use UTF-8)
-        logger.info("Writing merged data")
-        self.data.to_csv(output_path.with_suffix(".csv"), index=False, encoding="utf-8-sig")
-        self.data.to_feather(output_path.with_suffix(".feather"))
 
     def filter_records(self, field: str, value_list: list, mask: bool = False, exclusion_analysis: bool = False) -> None:
         """Filters the Census records by any field in ONS PD.
