790ec2a6a3290daa59a79ffa2378a18df8d068d5
AA-Turner <9087854+aa-turner@users.noreply.github.com>
2021-05-29 03:31:56 +0100

    Remove ScoutData.filter_records

--- src/incognita/data/scout_data.py
+++ src/incognita/data/scout_data.py
@@ -7,7 +7,6 @@ from pyarrow import feather
 
 from incognita.logger import logger
 from incognita.utility import config
-from incognita.utility import filter
 
 
 class ScoutData:
@@ -18,15 +17,3 @@ class ScoutData:
         start_time = time.time()
         self.census_data = feather.read_feather(config.SETTINGS.census_extract.merged) if load_census_data else pd.DataFrame()
         logger.info(f"Loaded Scout Census data, {time.time() - start_time:.2f} seconds elapsed.")
-
-    def filter_records(self, field: str, value_list: set, exclude_matching: bool = False, exclusion_analysis: bool = False) -> None:
-        """Filters the Census records by any field in ONS PD.
-
-        Args:
-            field: The field on which to filter
-            value_list: The values on which to filter
-            exclude_matching: If True, exclude the values that match the filter. If False, keep the values that match the filter.
-            exclusion_analysis:
-
-        """
-        self.census_data = filter.filter_records(self.census_data, field, value_list, exclude_matching, exclusion_analysis)
